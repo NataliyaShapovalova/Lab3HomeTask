a4e9503aa93d6409040d1ffb3eb1342500428f21
Nataliya <arnicaaliya@gmail.com>
2022-12-29 12:15:32 +0300

    выводим куб всех чисел до N

---
+++ Task23/Program.cs
@@ -0,0 +1,8 @@
+﻿// принимает на вход число N  и выдает таблицу кубов
+Console.Clear();
+Console.Write("Введите число для выведения таблицы кубов:");
+int a=Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= a; i++)
+{
+    Console.Write(i +"^"+"3 = "+" "+ Math.Pow(i,3));    
+}
