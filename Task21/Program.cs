99b398d3fed4f1f53788ab26ef749f9ad438dd2b
Nataliya <arnicaaliya@gmail.com>
2022-12-29 11:50:55 +0300

    расстояние по координатам 3д

---
+++ Task21/Program.cs
@@ -0,0 +1,16 @@
+﻿// координаты двух точек в 3д пространстве
+double Location(string name)
+{
+    System.Console.WriteLine(name);
+    double Ax= Convert.ToDouble(Console.ReadLine());
+    return Ax;
+}
+double Ax=Location("Введите Ax");
+double Ay=Location("Введите Ay");
+double Az=Location("Введите Az");
+double Bx=Location("Введите Bx");
+double By=Location("Введите By");
+double Bz=Location("Введите Bz");
+
+double AB= Math.Sqrt(Math.Pow((Bx-Ax),2)+Math.Pow((By-Ay),2)+Math.Pow((Bz-Az),2));
+Console.WriteLine(AB);
