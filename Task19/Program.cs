3cff4cf2a538e71386f3247cd7f7f937cab68477
Nataliya <arnicaaliya@gmail.com>
2022-12-29 10:37:40 +0300

    Палиндром

---
+++ Task19/Program.cs
@@ -0,0 +1,13 @@
+﻿// палиндром
+System.Console.WriteLine("Введите пятизначное число");
+int number =Convert.ToInt32(Console.ReadLine());
+
+    if (number.ToString().ToLower()==(new string(number.ToString().ToLower().Reverse().ToArray())))
+    {
+      Console.Write($"{number} -Палиндром"); 
+    }
+
+else
+{
+    Console.Write("Не палиндром");
+}
